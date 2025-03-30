55dafe71027f2b7bd046c0c4339c4268f2f42e6d
Alexander Kolev <165174435+aleksmkolev@users.noreply.github.com>
2025-03-30 21:52:08 +0100

    edit

---
+++ Softuni-React-Exam-main/client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h2>Welcome to our site</h2>
+    </>
+  )
+}
+
+export default App
