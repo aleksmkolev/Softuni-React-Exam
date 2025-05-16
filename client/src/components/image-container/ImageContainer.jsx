00066180eefb86fc65e38ed44b437a9f2ed2b68f
Alexander Kolev <165174435+aleksmkolev@users.noreply.github.com>
2025-05-16 21:55:18 +0100

    misc

--- client/src/components/image-container/ImageContainer.jsx
+++
@@ -1,7 +0,0 @@
-export default function ImageContainer({image}) {
-    return (
-        <div className="img-container">
-            <img src={image} alt="image from the marker" width="220px" />
-        </div>
-    );
-};
